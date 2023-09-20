0dc8ed133e1b2652c2f48b4030d07882d77ae8f9
JacopoCasanova98 <131349460+JacopoCasanova98@users.noreply.github.com>
2023-09-20 12:01:50 +0200

    Update login.js

--- react-f-end/src/pages/login.js
+++ react-f-end/src/pages/login.js
@@ -9,7 +9,7 @@ const Login = () => {
     const handleLoginSubmit = async (e) => {
         e.preventDefault();
         
-        const response = await fetch('http://localhost:3000/auth/login', {
+        const response = await fetch('https://aq-server.onrender.com/auth/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
