6e5ee2e1174ab384defc962fd33667afe4d970b6
JacopoCasanova98 <131349460+JacopoCasanova98@users.noreply.github.com>
2023-10-05 17:22:16 +0200

    Update userModel.js

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   username: String,
   password: String,
+  favoriteCities: [{ type: String }], // Array di città preferite
+  token: String,
 });
 
 module.exports = mongoose.model('User', userSchema);
+
