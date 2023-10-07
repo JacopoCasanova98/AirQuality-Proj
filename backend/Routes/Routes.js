cf452b34278b898b178bf317dc4d862045f43c2c
JacopoCasanova98 <131349460+JacopoCasanova98@users.noreply.github.com>
2023-10-07 13:51:33 +0200

    Update Routes.js

--- backend/Routes/Routes.js
+++ backend/Routes/Routes.js
@@ -2,10 +2,24 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/airqualityController');
 const favoriteController = require('../controllers/favoriteController');
+const axios = require('axios'); // Importa la libreria Axios per effettuare richieste HTTP
 
 
-router.get('https://aq-server.onrender.com/', (req, res) => {
-   console.log('Get req OK');
+router.get('https://aq-server.onrender.com', async (req, res) => {
+  try {
+    // Effettua una richiesta GET a https://aq-server.onrender.com
+    const response = await axios.get('https://aq-server.onrender.com');
+
+    // Se la richiesta va a buon fine, stampa un messaggio di conferma
+    console.log('Tutto OK: ', response.data);
+
+    // Invia una risposta al client
+    res.status(200).json({ message: 'Tutto OK' });
+  } catch (error) {
+    // In caso di errore, gestisci l'errore e invia una risposta di errore al client
+    console.error('Errore nella richiesta GET: ', error.message);
+    res.status(500).json({ message: 'Errore nella richiesta GET' });
+  }
 });
 
 
